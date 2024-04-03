110fc4368bd82a9ce3357e0ce3e5e8eb6f0ae8d4
trungnt173 <trungnt173@fpt.edu.vn>
2024-04-03 07:38:37 +0700

    update

--- controller/ProductListController.js
+++ controller/ProductListController.js
@@ -15,18 +15,16 @@ window.ProductListController = function ($scope,$http, $location) {
 		})
 	}
 	$scope.getData();
-	$scope.onDelete = function(deleteID) {
-		let confirm = window.confirm("Bạn có muốn xóa không ?");
-		// nếu như confirm ok tức là đồng ý xóa
-		if(confirm) {
-			$http.delete(`${apiUrl}/${deleteID}`).then(
-				function(response) {
-					if(response.status == 200) {
-						//gọi lại hàm getData để cập nhập lại bảng
+	$scope.onDelete = function(id){
+		let confirm = window.confirm("Bạn có chắc chắn muốn xóa hay không??");
+		if(confirm){
+			$scope.products =  $scope.products.filter(function(item){
+				$http.delete(`${apiUrl}/${id}`).then(function(response){
+					if (response.status == 200){
 						$scope.getData();
 					}
-				}
-			)
+				})
+			})
 		}
 	}
 	$scope.onEdit = function (id) {
